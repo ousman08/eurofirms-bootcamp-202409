fce0041cf6e1c0cfbf3d9b215e8e642cea2c9852
ousman <oman7910@gmail.com>
2024-10-05 16:50:42 +0200

    add many elements in one call

--- staff/ousman-kruball/playground/array/Array.prototype.push.js
+++ staff/ousman-kruball/playground/array/Array.prototype.push.js
@@ -3,10 +3,10 @@ console.log('TEST Array.prototype.push')
 console.log('CASE and 400 to nums')
 
 var nums = [100, 200, 300]
-var length = nums.push(400)
+var length = nums.push(400) 
 console.log(nums)
 // [100, 200, 300 400] (4)
-console.log(length)
+console.log(length) 
 //4
 
 console.log('CASE and f to chars')
@@ -17,3 +17,13 @@ console.log(chars)
 // ['A', 'B', 'C', 'D', 'E', 'F'] (6)
 console.log(length)
 // 6
+
+console.log('Case add many elements in one call')
+
+// 
+var colors = ['red', 'green', 'blue', 'magenta']
+var length = colors.push('cyan', 'yellow', 'orange', 'pink')
+console.log(colors)
+//['red', 'green', 'blue', 'magneta', 'cyan', 'yellow', 'orange', 'pink']
+console.log(length)
+//8
