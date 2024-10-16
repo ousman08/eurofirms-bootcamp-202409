95d1f45948bd93884bf0812ae84470d111396bc3
ousman <oman7910@gmail.com>
2024-10-16 20:08:12 +0200

    add changes to App #103

--- staff/ousman-kruball/app/main.js
+++ staff/ousman-kruball/app/main.js
@@ -1,8 +1,8 @@
         var users = []
  
-        user[0] = { name: 'ji Rafa', email: 'ji@rafa.com', username: 'jirafa', password: '123123123'}
-
-
+        users[0] = { name: 'Ji Rafa', email: 'ji@rafa.com', username: 'jirafa', password: '123123123' }
+        users[1] = { name: 'Ele Fante', email: 'ele@fante.com', username: 'elefante', password: '123123123' }
+        users[2] = { name: 'Coco Drilo', email: 'coco@drilo.com', username: 'cocodrilo', password: '123123123' }
 
         var sections = document.querySelectorAll('section')
 
@@ -101,7 +101,7 @@
             loginSection.style.display = ''
         })
 
-        var loginForm = loginSection.queryselectorAll('form')
+        var loginForm = loginSection.querySelector('form')
 
         loginForm.addEventListener('submit', function (event) {
             event.preventDefault()
