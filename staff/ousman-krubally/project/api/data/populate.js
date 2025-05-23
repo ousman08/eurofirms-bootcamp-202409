67cd92853134e7f8148bd6ec5bee5442a05f0257
ousman <oman7910@gmail.com>
2025-05-23 16:32:00 +0200

    populate #146

--- staff/ousman-krubally/project/api/data/populate.js
+++ staff/ousman-krubally/project/api/data/populate.js
@@ -14,23 +14,26 @@ mongoose.connect('mongodb://127.0.0.1:27017/mattas')
 
         const peter = new User({ name: 'Peter Pan', email: 'peter@pan.com', username: 'peterpan', password: '123123123' })
 
-        return admin.save()
+        const jatru = new User({ name: 'jatru', email: 'jatru@man', username: 'jatru', password: '123123123' })
 
-            .then((savedAdmin) => {
-                const adminPost = new Product({ author: savedAdmin._id, image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Chocolate.jpg/250px-Chocolate.jpg', title: 'chocolate', description: 'new chocolate', category: 'dessert', date: new Date('2025-03-11') })
+        const adminPost = new Product({ author: admin._id, image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Chocolate.jpg/250px-Chocolate.jpg', title: 'chocolate', description: 'new chocolate', category: 'dessert', date: new Date('2025-03-11') })
 
-                return Promise.all([
-                    savedAdmin.save(),
-                     adminPost.save(),
-                     peter.save()
-                ])
+        const adminPost1 = new Product({ author: admin._id, image: 'https://www.tasteofhome.com/wp-content/uploads/2019/05/Fried-Ice-Cream-Dessert-Bars-_EXPS_SDJJ19_232652_B02_06_1b_rms-2.jpg', title: 'fried ice cream', description: 'esta riquisimo', category: 'dessert', date: new Date('2025-03-22') })
 
-            })
+        return Promise.all([
+            adminPost1.save(),
+            jatru.save(),
+            admin.save(),
+            adminPost.save(),
+            peter.save()
+        ])
+
     })
-    .then(items => {
-        const [admin, adminPost, peter] = items
 
-        console.log(admin, adminPost, peter)
+    .then(items => {
+        const [admin, adminPost, peter, jatru, adminPost1] = items
+
+        console.log(admin, adminPost, peter, jatru, adminPost1)
     })
 
     .then(() => console.log('then end'))
